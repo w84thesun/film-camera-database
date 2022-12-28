cacf8a34658fa71e126b969ea53f3c718003fadf
Dmitry <dmitry.eremenko@ferretdb.io>
2022-12-28 02:06:26 +0000

    wip

---
+++ tools/generate.go
@@ -0,0 +1,11 @@
+//go:build go1.19
+
+package tools
+
+import (
+	_ "github.com/go-task/task/v3/cmd/task"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+)
+
+//go:generate go build -v -o ../bin/ github.com/go-task/task/v3/cmd/task
+//go:generate go build -v -o ../bin/ github.com/golangci/golangci-lint/cmd/golangci-lint
